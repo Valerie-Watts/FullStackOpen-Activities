a984a5c56c753bb10d63241dbab0d103c9145829
ChknMcNuggs <valerie.watts@edu.uag.mx>
2026-05-11 07:55:19 -0600

    Part 1.8

--- Pt1/unicafe/src/App.jsx
+++ Pt1/unicafe/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {props.good}</p>
+        <p>neutral {props.neutral}</p>
+        <p>bad {props.bad}</p>
+        <p>all {props.good + props.neutral + props.bad}</p>
+        <p>average {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</p>
+        <p>positive {props.good / (props.good + props.neutral + props.bad) * 100} %</p>
+      </div>
+    )
+  }
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
@@ -12,14 +27,8 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
 
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good / (good + neutral + bad) * 100} %</p>
     </div>
   )
 }
