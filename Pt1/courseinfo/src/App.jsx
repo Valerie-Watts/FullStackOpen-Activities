f334f4bf6be5616d052fb0f09c9ca6b8c436114f
ChknMcNuggs <valerie.watts@edu.uag.mx>
2026-05-11 07:29:33 -0600

    Part 1.4

--- Pt1/courseinfo/src/App.jsx
+++ Pt1/courseinfo/src/App.jsx
@@ -13,9 +13,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
@@ -43,22 +43,20 @@ const Total = (props) =>{
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = { name: 'Fundamentals of React', exercises: 10 }
-  const part2 = { name: 'Using props to pass data', exercises: 7 }
-  const part3 = { name: 'State of a component', exercises: 14 }
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
+      <Content parts={parts} />
       <Total
-        ex1={part1.exercises}
-        ex2={part2.exercises}
-        ex3={part3.exercises}
+        ex1={parts[0].exercises}
+        ex2={parts[1].exercises}
+        ex3={parts[2].exercises}
       />
     </div>
   )
