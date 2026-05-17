f7069cef525c28b953718605e51c8fe409f6946d
ChknMcNuggs <valerie.watts@edu.uag.mx>
2026-05-16 18:19:59 -0600

    Part 3.8

--- Pt3/phonebook-backend/index.js
+++ Pt3/phonebook-backend/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   { id: 1, name: 'Arto Hellas', number: '040-123456' },
